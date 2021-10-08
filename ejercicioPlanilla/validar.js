6127e34cde6200e03dd05688aaca7e8dc19f1e50
yulynegrette <ynegrette28@gmail.com>
2021-10-08 14:09:24 -0500

    cambios2

---
+++ ejercicioPlanilla/validar.js
@@ -0,0 +1,25 @@
+function validar_formulario(){
+    //1. accedemos a los elementos del formulario
+    var usuario = document.getElementById('username');
+    var correo = document.getElementById('email');
+    var contraseña = document.getElementById('password');
+    var mensaje ="";
+
+    //2. aplicar validaciones
+    if (!usuario.checkValidity()){
+        mensaje += "Usuario requerido y longitud minima 8 caracteres"
+    }
+
+    if (!correo.checkValidity()){
+        mensaje += "Correo requerido con formato xxx@xxx.xx"
+    }
+
+    if (!contraseña.checkValidity()){
+        mensaje += "Contraseña requerida y longitud minima 8 caracteres"
+    }
+
+    if (mensaje == ""){
+        mensaje = "Todos los campos del formulario son validos"
+    }
+    alert(mensaje)
+}
